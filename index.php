4aef03004cf5f01f6eb665b10e615b3972f1f7d7
Daathing <kilian.lefevre.1906@gmail.com>
2023-04-06 20:45:39 +0200

    révision de la boucle foreach et du tableau.
    Co-authored-by: Kévin Monteiro <Keirodev@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<!doctype php>
-<php lang="fr">
+<!doctype html>
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -11,207 +11,24 @@
 <body>
     <?php 
     include('header.php');
+    include_once('oeuvres.php');
     ?>
     <main>
         <div id="liste-oeuvres">
-            <article class="oeuvre">
-                <a href="oeuvre-1.php">
-                    <?php 
-                        include('oeuvres.php');
-                        foreach ($oeuvres as $infos) {
-                        echo $oeuvre1['picture'];
-                        echo $oeuvre1['title'];
-                        echo $oeuvre1['artist'];
-                        break;
-                    }
-                    ?>
-                </a>
-                <a href="oeuvre.php?$oeuvre1"></a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-2.php">
-                    <?php 
-                        include('oeuvres.php');
-                        foreach ($oeuvres as $infos) {
-                        echo $oeuvre2['picture'];
-                        echo $oeuvre2['title'];
-                        echo $oeuvre2['artist'];
-                        break;
-                    }
-                    ?>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-3.php">
-                    <?php 
-                        include('oeuvres.php');
-                        foreach ($oeuvres as $infos) {
-                        echo $oeuvre3['picture'];
-                        echo $oeuvre3['title'];
-                        echo $oeuvre3['artist'];
-                        break;
-                    }
-                    ?>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-4.php">
-                    <?php 
-                        include('oeuvres.php');
-                        foreach ($oeuvres as $infos) {
-                        echo $oeuvre4['picture'];
-                        echo $oeuvre4['title'];
-                        echo $oeuvre4['artist'];
-                        break;
-                    }
-                    ?>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-5.php">
-                    <?php 
-                        include('oeuvres.php');
-                        foreach ($oeuvres as $infos) {
-                        echo $oeuvre5['picture'];
-                        echo $oeuvre5['title'];
-                        echo $oeuvre5['artist'];
-                        break;
-                    }
-                    ?>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-6.php">
-                    <?php 
-                        include('oeuvres.php');
-                        foreach ($oeuvres as $infos) {
-                        echo $oeuvre6['picture'];
-                        echo $oeuvre6['title'];
-                        echo $oeuvre6['artist'];
-                        break;
-                    }
-                    ?>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-7.php">
-                    <?php 
-                        include('oeuvres.php');
-                        foreach ($oeuvres as $infos) {
-                        echo $oeuvre7['picture'];
-                        echo $oeuvre7['title'];
-                        echo $oeuvre7['artist'];
-                        break;
-                    }
-                    ?>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-8.php">
-                    <?php 
-                        include('oeuvres.php');
-                        foreach ($oeuvres as $infos) {
-                        echo $oeuvre8['picture'];
-                        echo $oeuvre8['title'];
-                        echo $oeuvre8['artist'];
-                        break;
-                    }
-                    ?>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-9.php">
-                    <?php 
-                        include('oeuvres.php');
-                        foreach ($oeuvres as $infos) {
-                        echo $oeuvre9['picture'];
-                        echo $oeuvre9['title'];
-                        echo $oeuvre9['artist'];
-                        break;
-                    }
-                    ?>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-10.php">
-                    <?php 
-                        include('oeuvres.php');
-                        foreach ($oeuvres as $infos) {
-                        echo $oeuvre10['picture'];
-                        echo $oeuvre10['title'];
-                        echo $oeuvre10['artist'];
-                        break;
-                    }
-                    ?>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-11.php">
-                    <?php 
-                        include('oeuvres.php');
-                        foreach ($oeuvres as $infos) {
-                        echo $oeuvre11['picture'];
-                        echo $oeuvre11['title'];
-                        echo $oeuvre11['artist'];
-                        break;
-                    }
-                    ?>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-12.php">
-                    <?php 
-                        include('oeuvres.php');
-                        foreach ($oeuvres as $infos) {
-                        echo $oeuvre12['picture'];
-                        echo $oeuvre12['title'];
-                        echo $oeuvre12['artist'];
-                        break;
-                    }
-                    ?>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-13.php">
-                    <?php 
-                        include('oeuvres.php');
-                        foreach ($oeuvres as $infos) {
-                        echo $oeuvre13['picture'];
-                        echo $oeuvre13['title'];
-                        echo $oeuvre13['artist'];
-                        break;
-                    }
-                    ?>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-14.php">
-                    <?php 
-                        include('oeuvres.php');
-                        foreach ($oeuvres as $infos) {
-                        echo $oeuvre14['picture'];
-                        echo $oeuvre14['title'];
-                        echo $oeuvre14['artist'];
-                        break;
-                    }
-                    ?>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-15.php">
-                    <?php 
-                        include('oeuvres.php');
-                        foreach ($oeuvres as $infos) {
-                        echo $oeuvre15['picture'];
-                        echo $oeuvre15['title'];
-                        echo $oeuvre15['artist'];
-                        break;
-                    }
-                    ?>
-                </a>
-            </article>
+            <?php
+                foreach($oeuvres as $infos) {
+                    echo   '<article class="oeuvre">
+                                    <a href="oeuvre.php?id=KEY">
+                                        '.$infos['picture'].'
+                                        <h2> '.$infos['title'].' </h2>
+                                        <p class="description"> '.$infos['artist'].' </p>
+                                    </a>
+                                <a href="oeuvre.php?$oeuvre1"></a>
+                            </article>';
+                }
+            ?>
         </div>
     </main>
     <?php include('footer.php'); ?>
 </body>
-</php>
+</html>
